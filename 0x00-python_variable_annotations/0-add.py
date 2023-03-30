069e45d1467b91d56ecc4ec26a898f890e1d2d04
Awet11-Tesfay <hailegb2021@gmail.com>
2023-03-30 14:08:55 +0300

    basic_annotation_add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+''' Descripton: type-annotated function add that takes a float and float b as agruments and return their sum as a float
+    Arguments: a: float
+                b: float
+
+'''
+def add(a: float, b: float) -> float:
+    return a + b
