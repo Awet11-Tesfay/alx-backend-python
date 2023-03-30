93ad7c43dce4f6eae25d852ba21e86386584a4a9
Awet11-Tesfay <hailegb2021@gmail.com>
2023-03-30 17:22:25 +0300

    list_of_floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' Description- annoted function which takes a list of floats and returns a sum of the list
+    Arguments- input_list: float
+'''
+from math import addition
+
+def sum_list(input_list: list[float]) -> float:
+    ''' Return sum of the list '''
+    return addition(list)
