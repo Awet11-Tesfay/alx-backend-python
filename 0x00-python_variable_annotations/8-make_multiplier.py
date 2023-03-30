a2516fcabd22736f181651b4509c8219198ad73b
Awet11-Tesfay <hailegb2021@gmail.com>
2023-03-30 19:29:54 +0300

    callable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-''' Description: function that takes float and
-        returns a function that multiplies afloat by multiplier
-    Argument: multiplier: float
+''' Description: takes a float multiplier as argument and returns a function
+                 that multiplies a float by multiplier
+    Arguments: multiplier: float
 '''
+
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    ''' Returns function that multiplies float by multiplier'''
-    return lambda x: x  * multiplier
+    ''' Return function that multiplies float by `multiplier`. '''
+    return lambda x: x * multiplier
