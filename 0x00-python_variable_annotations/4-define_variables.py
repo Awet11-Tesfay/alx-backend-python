567307207b41fd59d910c5e61f50e232239ec21a
Awet11-Tesfay <hailegb2021@gmail.com>
2023-03-30 17:05:00 +0300

    variable_annotations

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 ''' Description define variables with specified values
-    Arguments: a: int with value of 1
-                pi: float with a value of 3.14
-                i_understand_annotations: a boolean with a value of True
-                school: a string with value of "Holberton"
+    Arguments: a- int with value of 1
+                pi- float with a value of 3.14
+                i_understand_annotations- a boolean with a value of True
+                school- a string with value of "Holberton"
 '''
+
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotation: bool = True
-school: str = "Holberton"
+school: str = 'Holberton'
