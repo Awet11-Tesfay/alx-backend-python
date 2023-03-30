4650ef834f29671627e8eb1d24beb0c10e7b89de
Awet11-Tesfay <hailegb2021@gmail.com>
2023-03-30 15:35:48 +0300

    concat_annotation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,4 +1,4 @@
-#!/bin/usr/python3
+#!/bin/usr/env python3
 ''' Description: type-annotated function add that takes a float a and a float b
                  as arguments and returns their sum as a float
     Arguments: str1: str
@@ -7,4 +7,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    ''' Returns concatenation of str1 and str2 '''
     return str1 + str2
