db519dd6a0eea5d7a1ed777b81fb2fe4f0a66f46
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-03 23:04:17 +0300

    concurrent

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -13,5 +13,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     ''' waits for random delay until max_delay '''
-    
+
     return sorted([await wait_random(max_delay) for i in range(n)])
