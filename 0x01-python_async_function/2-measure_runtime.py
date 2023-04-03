e6832ddc31ba321926fc9c94dd6a8d4b3c6b1a67
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-03 23:17:22 +0300

    measure_time

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,8 +11,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 def measure_time(n: int, max_delay: int) -> float:
     ''' Returns execution time for wait_n given n and mex_delay. '''
-    time_1 = time()
+    beg = time()
     run(wait_n(n, max_delay))
-    time_2 = time()
-    elipsed = time_2 - time_1
-    return elipsed / n
+    return (time() - beg) / n
