44851e17434e6ea838ae70619ac6e2766ab6c654
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-03 21:42:07 +0300

    concurrent

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ''' Description: Import wait_random from the previous python file
-                 you have written and write an async routine
+         you have written and write an async routine
     Arguments: n: int, max_delay: int = 10
     Returns: Lis: sorted list of delays as float
 '''
@@ -13,4 +13,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     ''' waits for random delay until max_delay '''
-    return sorted([await wait_random(max_delay) for i in range()])
+    return sorted([await wait_random(max_delay) for i in range(n)])
