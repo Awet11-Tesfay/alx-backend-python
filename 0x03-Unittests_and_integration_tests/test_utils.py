6ecd4d8d141101bb7d1e465afc61475def6ac109
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-23 08:33:27 +0300

    utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" Parameterize a unit test
+""" Write the first unit test for utils.access_nested_map
 """
 
 from parameterized import parameterized
-from unittest.mock import Mock, patch, mock
+from unittest.mock import Mock, patch
 import unittest
+from unittest import mock
 from utils import access_nested_map, memoize, get_json
 
 
@@ -16,6 +17,6 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ Return output test
+        """ Method to test that the method return what it is supposed
         """
         self.assertEqual(access_nested_map(map, path), expected_output)
